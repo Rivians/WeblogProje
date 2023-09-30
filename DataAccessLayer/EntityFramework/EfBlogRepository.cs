cdc28bc1b12b2f454fc400f42ff536c4cd573089
Semih Yazar <semih_114yazar@hotmail.com>
2023-09-30 17:36:04 +0300

    Added Eager Loading

--- DataAccessLayer/EntityFramework/EfBlogRepository.cs
+++ DataAccessLayer/EntityFramework/EfBlogRepository.cs
@@ -1,6 +1,8 @@
 ﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.Repositories;
 using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,5 +13,14 @@ namespace DataAccessLayer.EntityFramework
 {
 	public class EfBlogRepository : GenericRepository<Blog>, IBlogDal
 	{
+		public List<Blog> GetListWithCategory()
+		{
+			using(var c = new Context())
+			{
+				// Buradaki Include metodu, Entity Framework tarafından sunulan bir işlemdir ve bu kod parçası verileri yükleme (eager loading) işlemini gerçekleştirir.
+				// Blogs tablosundaki her bir blog nesnesini çekerken, ilişkili Category tablosundaki verileri de çeker. Bu, daha sonra kullanılacak olan bu verilere erişimin kolaylaştırılmasını sağlar.
+				return c.Blogs.Include(x => x.Category).ToList();
+			}
+		}
 	}
 }
