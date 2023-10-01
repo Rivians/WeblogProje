e5ef763e120070021fc6f35ed7f0f327dad28ef3
Semih Yazar <semih_114yazar@hotmail.com>
2023-10-02 02:08:16 +0300

    Added Register Controller and View

---
+++ WeblogProje/Controllers/RegisterController.cs
@@ -0,0 +1,27 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WeblogProje.Controllers
+{
+	public class RegisterController : Controller
+	{
+		WriterManager wm = new WriterManager(new EfWriterRepository());
+
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Index(Writer writer)
+		{
+			writer.WriterStatus = true;
+			writer.WriterAbout = "deneme";
+			wm.WriterAdd(writer);
+			return RedirectToAction("Index", "Blog");
+		}
+	}
+}
