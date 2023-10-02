a103842c2cc70099ff748c99b25e3ecd0feb3a42
Semih Yazar <semih_114yazar@hotmail.com>
2023-10-02 04:32:37 +0300

    Added GetBlogListByWriter + Writer/Blog Relation and Migration

---
+++ WeblogProje/ViewComponents/Blog/WriterLastBlog.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WeblogProje.ViewComponents.Blog
+{
+	public class WriterLastBlog : ViewComponent
+	{
+		BlogManager bm = new BlogManager(new EfBlogRepository());
+
+		public IViewComponentResult Invoke(int id)
+		{
+			var values = bm.GetBlogListByWriter(id);
+			return View(values);
+		}
+	}
+}
