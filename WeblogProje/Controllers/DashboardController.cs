41416db0b1dfd0a73a7c4730929758e015b4e828
Semih Yazar <semih_114yazar@hotmail.com>
2023-10-11 07:01:04 +0300

    Added Dashboard Stat Datas // Added WriterEditProfile

--- WeblogProje/Controllers/DashboardController.cs
+++ WeblogProje/Controllers/DashboardController.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WeblogProje.Controllers
 {
 	public class DashboardController : Controller
 	{
+		[AllowAnonymous]
 		public IActionResult Index()
 		{
+			Context c = new Context();
+			ViewBag.v1 = c.Blogs.Count().ToString();
+			ViewBag.v2 = c.Blogs.Where(x => x.WriterID == 1).Count().ToString();
+			ViewBag.v3 = c.Categories.Count().ToString();
+
 			return View();
 		}
 	}
--- WeblogProje/Controllers/WriterController.cs
+++ WeblogProje/Controllers/WriterController.cs
@@ -1,10 +1,17 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WeblogProje.Controllers
 {
 	public class WriterController : Controller
 	{
+		WriterManager wm = new WriterManager(new EfWriterRepository());
+
 		public IActionResult Index()
 		{
 			return View();
@@ -22,6 +29,34 @@ namespace WeblogProje.Controllers
 		public PartialViewResult WriterFooterPartial()
 		{
 			return PartialView();
+		}
+
+		[AllowAnonymous]
+		[HttpGet]
+		public IActionResult WriterEditProfile()
+		{
+			var values = wm.TGetByID(1);	
+			return View(values);
+		}
+
+		[HttpPost]
+		public IActionResult WriterEditProfile(Writer p)
+		{
+			WriterValidator wl = new WriterValidator();
+			ValidationResult result = wl.Validate(p);
+			if(result.IsValid)
+			{
+				wm.TUpdate(p);
+				return RedirectToAction("Index", "Dashboard");
+			}
+			else
+			{
+				foreach (var item in result.Errors)
+				{
+					ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+				}
+			}
+			return View();
 		}
 	}
 }
