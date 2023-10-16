7be4fe92f19cfb6ee488fa257245f1848abb55a6
Semih Yazar <semih_114yazar@hotmail.com>
2023-10-17 01:01:03 +0300

    Message sınıfı eklendi

--- DataAccessLayer/Migrations/20231016214423_mig_message_add.Designer.cs
+++ DataAccessLayer/Migrations/20231016214423_mig_message_add.Designer.cs
@@ -4,6 +4,7 @@ using DataAccessLayer.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DataAccessLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20231016214423_mig_message_add")]
+    partial class migmessageadd
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
